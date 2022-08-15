9ccf588084e83d052ce53d7228dbed1251224947
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-08-15 09:01:47 +0700

    setup appuser

---
+++ src/main/java/rifqimuhammadaziz/springauth/appuser/AppUserRepository.java
@@ -0,0 +1,13 @@
+package rifqimuhammadaziz.springauth.appuser;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository {
+
+    Optional<AppUser> findByEmail(String email);
+}
