978b09148ab20bd2a916f89f260f0432a94b9ca4
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-08-15 11:04:18 +0700

    setup registration service

--- src/main/java/rifqimuhammadaziz/springauth/registration/RegistrationController.java
+++ src/main/java/rifqimuhammadaziz/springauth/registration/RegistrationController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import rifqimuhammadaziz.springauth.appuser.AppUserService;
 
 @RestController
 @RequestMapping(path = "api/v1/registration")
@@ -15,7 +14,7 @@ public class RegistrationController {
     private final RegistrationService registrationService;
 
     @PostMapping
-    public String register(@RequestBody RegistrationService request) {
+    public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
 }
--- src/main/java/rifqimuhammadaziz/springauth/registration/RegistrationService.java
+++ src/main/java/rifqimuhammadaziz/springauth/registration/RegistrationService.java
@@ -1,11 +1,32 @@
 package rifqimuhammadaziz.springauth.registration;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import rifqimuhammadaziz.springauth.appuser.AppUser;
+import rifqimuhammadaziz.springauth.appuser.AppUserRole;
+import rifqimuhammadaziz.springauth.appuser.AppUserService;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
 
-    public String register(RegistrationService request) {
-        return "it works";
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
+    public String register(RegistrationRequest request) {
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email not valid");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
