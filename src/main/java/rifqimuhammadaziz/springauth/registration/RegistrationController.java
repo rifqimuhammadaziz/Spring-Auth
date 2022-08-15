99e2575baed84004fb56bdd1fa0296a1b2afaeb0
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-08-15 10:42:06 +0700

    setup security & test

---
+++ src/main/java/rifqimuhammadaziz/springauth/registration/RegistrationController.java
@@ -0,0 +1,21 @@
+package rifqimuhammadaziz.springauth.registration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rifqimuhammadaziz.springauth.appuser.AppUserService;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+
+    private final RegistrationService registrationService;
+
+    @PostMapping
+    public String register(@RequestBody RegistrationService request) {
+        return registrationService.register(request);
+    }
+}
---
+++ src/main/java/rifqimuhammadaziz/springauth/registration/RegistrationService.java
@@ -0,0 +1,11 @@
+package rifqimuhammadaziz.springauth.registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+
+    public String register(RegistrationService request) {
+        return "it works";
+    }
+}
